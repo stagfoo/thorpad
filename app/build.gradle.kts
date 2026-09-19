d5dcb35f261bcfb327374242497b43cbd641ab80
al <alexanderddking@gmail.com>
2026-09-20 09:16:24 +1000

    Release 1.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.thorpad.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0.16"
+        versionCode = 18
+        versionName = "1.0.17"
     }
 
     signingConfigs {
