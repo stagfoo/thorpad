0be2fc48e1f0ead6a33350b7d5c7a6c50778605a
al <alexanderddking@gmail.com>
2026-09-19 11:24:07 +1000

    Release 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.thorpad.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     signingConfigs {
